2301b71697294c080b7f49ac1f99f1be4ba27a37
Jonathan Cacace <jonathan.cacace@gmail.com>
2018-01-19 13:31:05 +0100

    talker bug fixed

--- talker.m
+++ talker.m
@@ -1,8 +1,8 @@
 %ROS_MASTER_URI
 rosshutdown
-ros_master_ip = 'http://192.168.1.5:11311';
+ros_master_ip = 'http://192.168.56.101:11311';
 %ROS_HOSTNAME
-matlab_ip = '192.168.1.13';
+matlab_ip = '192.168.56.1';
 %Connect to an external ROS Network, setting ROS_MASTER_URI and
 %ROS_HOSTNAME
 rosinit(ros_master_ip, 'NodeHost', matlab_ip);
@@ -11,7 +11,8 @@ pause(2) % wait a bit the roscore initialization
 talker_sub = rossubscriber( '/talker' );
 %Advertise a String on topic /chatter
 [chatter_pub, chatter_msg] = rospublisher('/chatter','std_msgs/String');
-r = rosrate(2); % 2 Hz loop rate 
+r = rosrate(2); % 2 Hz loop rate
+pause(2) % wait a bit the roscore initialization
 for i = 1:20
     %Get data from the input topic
     data = talker_sub.LatestMessage;
